f27138731455ee5cb0044dc07dfc87eaa19ec4c2
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-01-02 23:29:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+{
+    let tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            });
+        });
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="task__item">
+              <button class="task__button js-done">
+                ${task.done ? "✓" : ""}
+              </button>
+              <span class="task__${task.done ? " task__item--done" : ""}">
+                ${task.content}
+              </span>
+              <button class="task__button task__button--special js-remove">
+                🗑
+              </button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        }
+        newTaskElement.focus()
+
+    };
+
+    const init = () => {
+        render();
+        const form = document.querySelector(".js-form")
+        form.addEventListener("submit", onFormSubmit)
+        render();
+    };
+
+    init();
+}
